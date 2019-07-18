c1bd7746a819b34d65845305cedcaecd7110ca6b
Joseph Butterfield <raleigh9123@gmail.com>
2019-07-17 23:49:15 -0700

    Styles and Interval Timer
    Added random color generation, css changes, and interval timer.

--- js/script.js
+++ js/script.js
@@ -7,12 +7,7 @@ project 1 - A Random Quote Generator
 
 
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+  Quotes array with quote objects. Not every object contains a citation or a year, however every quote contains the quote text and source.
 ***/
 
 const quotes = [
@@ -20,51 +15,56 @@ const quotes = [
     quote: "You can never understand everything. But, you should push yourself to understand the system.",
     source: "Ryan Dahl",
     citation: "Creator of Node.js",
+    category: "Tech",
     year: ""
   },
   { 
     quote: "Before software can be reusable it first has to be usable.",
     source: "Ralph Johnson",
     citation: "",
+    category: "Tech",
     year: ""
   },
   {
     quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
     source: "Martin Fowler",
-    citation: "Author of numerous books on refactoring",
+    citation: "Author of Refactoring",
+    category: "Tech",
     year: ""
   },
   {
     quote: "... programming requires more concentration than other activities. It's the reason programmers get upset about 'quick interruptions' - such interruptions are tantamount to asking a juggler to keep three balls in the air and hold your groceries at the same time.",
     source: "Steven C. McConnell",
     citation: "Author of Code Complete",
+    category: "Tech",
     year: ""
   },
   {
     quote: "Why do we never have time to do it right, but always have time to do it over?",
     source: "Anonymous",
     citation: "",
+    category: "Tech",
     year: ""
   },
   {
     quote: "Simple things should be simple, complex things should be possible.",
-    source: "The Wiki Way: Quick Collaboration on the Web, Bo Leuf, Ward Cunningham",
+    source: "The Wiki Way: Quick Collaboration on the Web",
     citation: "",
+    category: "Tech",
     year: ""
   },
   {
     quote: "One principle problem of educating software engineers is that they will not use a new method until they believe it works and, more importantly, that they will not believe the method will work until they see it for themselves.",
     source: "Humphrey, W.S.",
     citation: "The Personal Software Process",
+    category: "Tech",
     year: ""
   }
 ]
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Use the random number to `return` a random quote object from the `quotes` array.
+  getRandomQuote()  generates a random number. The number is used to find the index of the quote array, thereby returning a random quote.
 ***/
 
 function getRandomQuote() {
@@ -73,43 +73,60 @@ function getRandomQuote() {
   return quote
 }
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote variable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
+/*** 
+ * Change background color
+ * generate random rgb number from 0 to 255
+ * select body and button
+ * set opacity to .8 and color to empty variable
+ * create random color by generating random numbers and printing an rgba(x,x,x,.8)
+ * set new color to button and body
 ***/
-
-function printQuote() {
-  let quote = getRandomQuote();
-  let theHTML = '';
-  theHTML += "<p class=\"quote\">" + quote.quote + "</p>";
-  theHTML += "<p class=\"source\">" + quote.source;
-  if (quote.citation) {
-    theHTML += "<span class=\"citation\">" + quote.citation + "</span>";
+function randomColor() {
+  function randomNumber() {
+    let number = Math.floor(Math.random() * 256);
+    return number
   }
-  if (quote.year) {
-    theHTML += "<span class=\"citation\">" + quote.year + "</span>";
-  }
-  theHTML += "</p>";
-  document.getElementById('quote-box').innerHTML = theHTML;
-  console.log(theHTML);
+  let body = document.getElementsByTagName("BODY")[0];
+  let button = document.getElementById('loadQuote');
+  
+  let opacity = .8;
+  let color = '';
+  color += "rgba(" + randomNumber() + "," + randomNumber() + "," + randomNumber() + "," + opacity +  ")";
+  button.style.backgroundColor = color;
+  body.style.backgroundColor = color;
 }
 
 /***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
+  printQuote() creates two <p> tags (and two <span> tags conditionally contained within the second <p> tag). 
+  The function executes and builds html text. This text is built via string concatenation using the random quote generated from getRandomQuote().
+  Finally, the function adds the content to the html element with id="quote-box".
+***/
+
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  //Build html content by declaring empty variable and building variable from randomQuote 's content
+  let theHTML = '';
+  theHTML += "<p class=\"quote\">" + randomQuote.quote + "</p>";
+  theHTML += "<p class=\"source\">" + randomQuote.source;
+  if (randomQuote.citation) {
+    theHTML += "<span class=\"citation\">" + randomQuote.citation + "</span>";
+  }
+  if (randomQuote.year) {
+    theHTML += "<span class=\"citation\">" + randomQuote.year + "</span>";
+  }
+  theHTML += "</p>";
+  //Load html into page when function executes
+  var quoteBox = document.getElementById('quote-box');
+  quoteBox.innerHTML = theHTML;
+  randomColor();
+}
+
+//Change Quote every 6 seconds.
+window.setInterval(printQuote,6000);
+
+/***
+  Find element with id="loadQuote" and add an eventlistener.
+  On click, execute function "printQuote"
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
